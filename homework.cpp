975ee024cdc10e87c24c2d2934a886f1943ca219
zverkovboris <32186297+zverkovboris@users.noreply.github.com>
2017-09-27 18:04:15 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -7,11 +7,10 @@ int main() {
     std::cout << "print 4 signed number a1a2a3a4" << std::endl;
     int o = 0;
     std::cin >> o;
-    int a1 = 0, a2 = 0, a3 = 0, a4 = 0;
-    a1 = o - (o / 10) * 10;
-    a2 = (o / 10) - (o / 100) * 10;
-    a3 = (o / 100) - (o / 1000) * 10;
-    a4 = o / 1000;
+    int a1 = o - (o / 10) * 10;
+    int a2 = (o / 10) - (o / 100) * 10;
+    int a3 = (o / 100) - (o / 1000) * 10;
+    int a4 = o / 1000;
     std::cout << "summ=a1+a2+a3+a4=" << a1 + a2 + a3 + a4 << std::endl;
     std::cout << "a1*a2*a3*a4=" << a1*a2*a3*a4 << std::endl;
     ////////////////////////
